09b84dd9a195d1395c6249b1affbcb21efff9cc3
Nicholas <nbjohnson10@gmail.com>
2019-11-21 22:45:54 -0500

    working on trying to fix upload video

--- src/main/java/com/cs3733kakistocrat/group/UploadVideoHandler.java
+++ src/main/java/com/cs3733kakistocrat/group/UploadVideoHandler.java
@@ -24,15 +24,20 @@ public class UploadVideoHandler implements RequestHandler<UploadVideoRequest,Upl
 	@Override
 	public UploadVideoResponse handleRequest(UploadVideoRequest input, Context context) {
 		UploadVideoResponse response;
-		
+		System.out.println(input.getVideoName() + input.getCharacter() + input.getSentence());
 		try {
+			System.out.println("Video file here");
+			System.out.println(input.getVideoFile());
 			byte[] encoded = java.util.Base64.getDecoder().decode(input.getVideoFile());
+			System.out.println("Here1");
 			Video video = new Video(input.getVideoName(), input.getCharacter(), input.getSentence());
+			System.out.println("Here2");
 			if (uploadVideo(input.getVideoName(), encoded, video)) {
 				response = new UploadVideoResponse(input.getVideoName());
 			} else {
 				response = new UploadVideoResponse(input.getVideoName(), 422);
 			}
+			System.out.println("Here3");
 		}catch (Exception e) {
 			response = new UploadVideoResponse("Unable to upload video: " + input.getVideoName()+ " (" + e.getMessage() + ")", 400);
 		}
@@ -42,20 +47,27 @@ public class UploadVideoHandler implements RequestHandler<UploadVideoRequest,Upl
 	boolean uploadVideo(String name, byte[]  contents, Video video) throws Exception {
 		if (logger != null) { logger.log("in uploadVideo"); }
 		VideosDAO dao = new VideosDAO();
-		
+		System.out.println("Here3");
 		if (s3 == null) {
-			logger.log("attach to S3 request");
+			System.out.println("Create s3");
+//			logger.log("attach to S3 request");
 			s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_2).build();
-			logger.log("attach to S3 succeed");
+			System.out.println("created s3");
+//			logger.log("attach to S3 succeed");
 		}
-		
+		System.out.println("Here4");
 		ByteArrayInputStream bais = new ByteArrayInputStream(contents);
 		ObjectMetadata omd = new ObjectMetadata();
 		omd.setContentLength(contents.length);
-		
+		System.out.println("Here5");
 		//in S3 bucket
+		try {
 		PutObjectResult res = s3.putObject(new PutObjectRequest("3733kakistocrat", "videos/" + name, bais, omd));
-		
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+			return false;
+		}
+		System.out.println("Here6");
 		//now have to actually upload to database 
 		//!!! might not be the actual url, need to test
 		String url = "3733kakistocrat/videos/" + name + bais + omd;
