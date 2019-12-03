619d0f4ef47339f8280a122dc131e6041f1b504f
Adrianna Staszewska <41236648+adriannaStaszewska@users.noreply.github.com>
2019-12-03 17:00:33 -0500

    RemoteRequest changes

--- src/main/java/com/cs3733kakistocrat/group/http/RemoveRemoteRequest.java
+++ src/main/java/com/cs3733kakistocrat/group/http/RemoveRemoteRequest.java
@@ -2,7 +2,16 @@ package com.cs3733kakistocrat.group.http;
 
 public class RemoveRemoteRequest {
 	String url;
+	String api_key;
 	
+	public String getApi_key() {
+		return api_key;
+	}
+
+	public void setApi_key(String api_key) {
+		this.api_key = api_key;
+	}
+
 	public RemoveRemoteRequest() {
 	}
 	
