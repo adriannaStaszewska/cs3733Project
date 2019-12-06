672e7971811835c97f87ce2f289102e6d2475f67
slwoodland <slwoodland@wpi.edu>
2019-12-06 16:08:36 -0500

    fixed ordering for append video

--- src/main/java/com/cs3733kakistocrat/group/database/VideoToPlaylistDAO.java
+++ src/main/java/com/cs3733kakistocrat/group/database/VideoToPlaylistDAO.java
@@ -34,7 +34,7 @@ public class VideoToPlaylistDAO {
             }
             resultSet.close(); 
             ps.close();
-    		  
+    		 
     		return videos;
     	} catch (Exception e) {
     		e.printStackTrace();
@@ -45,13 +45,13 @@ public class VideoToPlaylistDAO {
     public boolean appendVideoToPlaylist(Video video, Playlist playlist) throws Exception {
         try {
         	
-        	PreparedStatement ps1 = conn.prepareStatement("SELECT COUNT(*) FROM playlist_video WHERE playlist_name=? ORDER BY video_position asc;");
+        	PreparedStatement ps1 = conn.prepareStatement("SELECT MAX(video_position) FROM playlist_video WHERE playlist_name=?;");
         	ps1.setString(1, playlist.getPlaylistName());
 
         	ps1.execute();
         	ResultSet results = ps1.getResultSet();
         	results.next();
-        	int rowNum = results.getInt(1);
+        	int rowNum = results.getInt(1) + 1;
         	ps1.close();
         	
             PreparedStatement ps = conn.prepareStatement("INSERT INTO playlist_video (video_id, playlist_name, video_position) values(?,?,?);");
